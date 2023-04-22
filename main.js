d8e7a05b95da05c4734c5792d3c5c49c709efa2b
Henry Kolawole <you@example.com>
2023-04-22 08:04:38 +0100

    Home page is up and functional

---
+++ main.js
@@ -0,0 +1,29 @@
+let hamOpen = document.getElementById("Open");
+let hamClose =  document.getElementById("Close");
+
+let mobileMenu = document.getElementById("mobileMenu");
+// let body = document.body;
+
+
+hamOpen.addEventListener("click", ()=>  {
+
+    if (mobileMenu.classList.contains("hide")) {
+
+        mobileMenu.classList.remove("hide");
+        mobileMenu.classList.add("show");
+        // body.classList.add("noscroll")
+
+        hamClose.addEventListener("click", ()=> {
+
+            // console.log("Hi there");
+            if(mobileMenu.classList.contains("show")) {
+
+                mobileMenu.classList.remove("show");
+                mobileMenu.classList.add("hide");
+                // body.classList.remove("noscroll");
+
+            }
+        })
+    }
+
+})
